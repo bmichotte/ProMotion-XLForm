936b20149540fa0e41467164b5d4003d76f18494
Benjamin Michotte <bmichotte@gmail.com>
2015-07-25 18:42:44 +0200

    add and remove cells and sections

--- lib/ProMotion/XLForm/xl_form_screen.rb
+++ lib/ProMotion/XLForm/xl_form_screen.rb
@@ -116,6 +116,60 @@ module ProMotion
       reloadFormRow(cell)
     end
 
+    def add_cell(cell, opts={})
+      if opts[:before]
+        row = opts[:before]
+        if row.is_a?(Symbol)
+          row = cell_with_tag(row)
+        end
+        self.form.addFormRow(cell, beforeRow: row)
+      elsif opts[:after]
+        row = opts[:after]
+        if row.is_a?(Symbol)
+          row = cell_with_tag(row)
+        end
+        self.form.addFormRow(cell, afterRow: row)
+      else
+        mp "Don't know where to add cell, please provides either :before or :after", force_color: :red
+      end
+    end
+
+    def add_section(section, opts={})
+      if opts[:index]
+        self.form.addFormSection(section, atIndex: opts[:index])
+      elsif opts[:after]
+        self.form.addFormSection(section, afterSection: opts[:after])
+      else
+        self.form.addFormSection(section)
+      end
+    end
+
+    def remove_section!(section_or_index)
+      if section_or_index.is_a?(XLFormSectionDescriptor)
+        self.form.removeFormSection(section_or_index)
+      else
+        self.form.removeFormSectionAtIndex(section_or_index)
+      end
+    end
+
+    def remove_cell!(cell)
+      if cell.is_a?(Symbol)
+        self.form.removeFormRowWithTag(cell.to_s)
+      elsif cell.is_a?(String)
+        self.form.removeFormRowWithTag(cell)
+      else
+        self.form.removeFormRow(cell)
+      end
+    end
+
+    def enabled=(value)
+      self.form.disabled = !value
+    end
+
+    def enabled?
+      !self.form.isDisabled
+    end
+
     protected
     def on_cancel(_)
       form_options = self.class.get_form_options
